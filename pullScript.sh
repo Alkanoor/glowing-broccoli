e175d754e9ed4c593fcea93788e49006969a18ec
Telecom Robotics <telecom.robotics@gmail.com>
2016-03-05 18:25:22 +0100

    finished

--- pullScript.sh
+++ pullScript.sh
@@ -9,9 +9,9 @@ for i in $(find $2);
 do
   if [ "${i}" != "$2" ]
   then
-    file=$1/$(echo $(basename ${i}) | xxd -r -p | openssl enc -d -aes-256-cbc -pass file:$3) \
-    && head $i &>/dev/null \
+    file=$(echo $(basename ${i}) | xxd -r -p | openssl enc -d -aes-256-cbc -pass file:$3) \
+    && head $i 1>/dev/null 2>/dev/null\
     && mkdir -p $(dirname ${file}) && touch ${file}\
-    && cat $i | openssl enc -d -aes-256-cbc -salt -pass file:$3 -out ${file}
+    && cat $i | openssl enc -d -aes-256-cbc -salt -pass file:$3 > ${file}
   fi
 done
