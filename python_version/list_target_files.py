88d241ee601669cb148cfdc18fa25abd8f8c3c4b
Alkanoor <Alkanoor@github.com>
2016-09-10 14:57:05 +0200

    push script almost finished

--- python_version/list_target_files.py
+++ python_version/list_target_files.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+import binascii
 from util import *
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -28,9 +29,11 @@ def list_target_files(target_dir):
             ctr1 = enc[AES.block_size:2*AES.block_size]
             iv2 = enc[2*AES.block_size:3*AES.block_size]
             ctr2 = enc[3*AES.block_size:4*AES.block_size]
+            read_date_enc = enc[4*AES.block_size:5*AES.block_size]
             counter.reset_counter(ctr1)
             cipher = AES.new(key, AES.MODE_CTR, iv1, get_current_counter)
             filename = cipher.decrypt(base64_no_slash_decode(f[0]))
+            read_date = int(binascii.hexlify(cipher.decrypt(read_date_enc)),16)
         except Exception as e:
             print("[-] Error during AES deciphering of file "+full_path(f)+", please verify the key ("+str(e.args[0])+")")
             print("[+] It is possible to delete the file and continue process, but data may be lost. Do that only if you are sure of what you're doing!")
@@ -45,10 +48,28 @@ def list_target_files(target_dir):
                 print("[-] Aborting ...")
                 exit()
 
-        print(filename)
-        if isfile(filename):
-            ret.append((filename,int(os.path.getmtime(filename))))
-        else:
-            ret.append((filename,-1))
+        non_ascii = 0
+        for i in range(len(filename)):
+            if ord(filename[i])<32 or ord(filename[i])>127:
+                non_ascii += 1
+        if non_ascii > len(filename)//3:
+            print("[+] It is possible filename "+filename+" has been wrongly deciphered, please verify the key")
+            print("[+] Do you want to delete the problematic file ? (do that only if you know what it means) [N/y]")
+            i = raw_input()
+            if i == 'y':
+                os.remove(full_path(f))
+                print("[-] File "+full_path(f)+" deleted")
+                print("")
+                continue
+            else:
+                print("")
+
+        try:
+            if isfile(filename):
+                ret.append((filename,int(os.path.getmtime(filename)),full_path(f),read_date))
+            else:
+                ret.append((filename,-1,full_path(f),read_date))
+        except:
+            pass
 
     return ret
--- python_version/pushScript.py
+++ python_version/pushScript.py
@@ -9,6 +9,7 @@ import binascii
 from util import *
 from Crypto import Random
 from Crypto.Cipher import AES
+from list_target_files import *
 
 
 force_replace = False
@@ -29,9 +30,23 @@ extend_dir(dirs,files,('.',src))
 for i in range(len(dirs)):
     extend_dir(dirs,files,dirs[i])
 
-print(dirs)
-print(files)
+# print(dirs)
+# print(files)
 
+cur_encoded = list_target_files(dst)
+dict_cur_encoded = {}
+dict_to_delete_if_replace = {}
+
+for u,v,w,x in cur_encoded:
+    if dict_to_delete_if_replace.get(u) is not None:
+        os.remove(w)
+    else:
+        dict_cur_encoded[u] = (v,x)
+        dict_to_delete_if_replace[u] = w
+print(cur_encoded)
+print(files)
+print(dict_cur_encoded)
+print(dict_to_delete_if_replace)
 
 key = open('key','r').read()
 h = hashlib.sha256()
@@ -42,20 +57,29 @@ for f in files:
         raw = full_path(f)
         iv = Random.new().read(AES.block_size)
         ctr = Random.new().read(AES.block_size)
+        iv_ctr_name_enc = iv+ctr
         counter.reset_counter(ctr)
         cipher = AES.new(key, AES.MODE_CTR, iv, get_current_counter)
+
         enc = base64_no_slash_encode(cipher.encrypt(raw))
-        iv_ctr_name_enc = iv+ctr
+
+        date = format(int(os.path.getmtime(raw)),'x')
+        date = '0'*(AES.block_size*2-len(date))+date
+
+        assert(len(date)//2==AES.block_size)
+        date_enc = cipher.encrypt(binascii.unhexlify(date))
+        assert(len(date_enc)==AES.block_size)
     except Exception as e:
         print("[-] Error during AES ciphering : "+str(e.args[0]))
         print("[-] Aborting ...")
         exit()
-    if isfile(dst+"/"+enc):
-        cur_date = 1 #get date(cur_file)
-        pushed_date = 2 #get date(f)
+
+    existing = True
+    if dict_cur_encoded.get(full_path(f)) is not None:
+        cur_date,pushed_date = dict_cur_encoded[full_path(f)]
         replace = False
         if not force_replace:
-            if cur_date != f:
+            if cur_date != pushed_date:
                 if cur_date < pushed_date:
                     print("[+] Warning, current date is inferior as pushed one, are you sure that you want replace the file ? [N/y]")
                     i = raw_input()
@@ -65,12 +89,14 @@ for f in files:
                     replace = True
     else:
         replace = True
+        existing = False
+
     if replace or force_replace:
         content_file = open(full_path(f),'rb').read()
         try:
             g = open(dst+"/"+enc,'wb+')
         except Exception as e:
-            print("[-] Error during file replacement of "+dst+"/"+enc+" : "+str(e.args[0]))
+            print("[-] Error during file creation of "+dst+"/"+enc+" : "+str(e.args[0]))
             print("[-] Aborting ...")
             exit()
         try:
@@ -79,17 +105,20 @@ for f in files:
             ctr = Random.new().read(AES.block_size)
             counter.reset_counter(ctr)
             cipher = AES.new(key, AES.MODE_CTR, iv, get_current_counter)
-            print(binascii.hexlify(iv+ctr))
-            enc = iv+ctr+cipher.encrypt(raw)
+            enc_content = iv+ctr+date_enc+cipher.encrypt(raw)
         except Exception as e:
             print("[-] Error during AES ciphering of file "+full_path(f)+" : "+str(e.args[0]))
             print("[-] Aborting ...")
             exit()
         try:
-            print(binascii.hexlify(iv_ctr_name_enc))
-            g.write(base64.b64encode(iv_ctr_name_enc+enc))
-            print("[+] File replaced")
+            g.write(base64.b64encode(iv_ctr_name_enc+enc_content))
+            g.close()
+            if existing:
+                os.remove(dict_to_delete_if_replace[full_path(f)])
+                print("[+] File "+dict_to_delete_if_replace[full_path(f)]+" deleted and recreated at "+(dst+"/"+enc)+" for original file "+full_path(f))
+            else:
+                print("[+] File "+(dst+"/"+enc)+" created for original file "+full_path(f))
         except Exception as e:
-            print("[-] Error during file replacement of "+dst+"/"+enc+" : "+str(e.args[0]))
+            print("[-] Error during file creation of "+dst+"/"+enc+" : "+str(e.args[0])+" for original file "+full_path(f))
             print("[-] Aborting ...")
             exit()
